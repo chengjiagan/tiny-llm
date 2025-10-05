7469def65917e0fcbef89ffd74c63edecf62752d
Jiagan Cheng <waterlemongan@outlook.com>
2025-10-05 22:03:06 +0800

    done: week 1 day 5

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -3,10 +3,15 @@ import mlx.core as mx
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        assert weight.shape == (vocab_size, embedding_dim), f"expect weight's shape to be (vocab_size, embedding_dim), but get weight.shape={weight.shape}, vocab_size={vocab_size}, embedding_dim={embedding_dim}."
+
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        return mx.take(self.weight, indices=x, axis=0)
 
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        assert x.shape[-1] == self.embedding_dim, f"expect input x's last dimension to be {self.embedding_dim}, but get {x.shape[-1]}."
+        return x @ self.weight.transpose()
--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -3,7 +3,7 @@ from .basics import linear, silu
 from .attention import scaled_dot_product_attention_grouped
 from .layer_norm import RMSNorm
 from .positional_encoding import RoPE
-from typing import Any
+from typing import Any, List
 from .embedding import Embedding
 from .quantize import dequantize_linear
 
@@ -66,7 +66,7 @@ class Qwen2MultiHeadAttention:
     ) -> mx.array:
         assert len(x.shape) == 3, f"expect x to have 3 dimensions, but get {len(x.shape)}."
         B, L, E = x.shape
-        assert L < self.max_seq_len, f"expect input sequence length is less than max_seq_len ({self.max_seq_len}), but get {L}."
+        assert L <= self.max_seq_len, f"expect input sequence length to be less than or equal to max_seq_len ({self.max_seq_len}), but get {L}."
         assert E == self.input_size, f"expect input hidden size to be {self.input_size}, but get {E}."
 
         # generate q, k, v and apply rope
@@ -107,8 +107,8 @@ class Qwen2MLP:
     ):
         """
         N.. is zero or more dimensions for batches
-        E is hidden_size (embedding dimension of the model)
-        I is intermediate_size (dimension of the hidden layer in MLP)
+        E is hidden_size (embedding dimension of the model) (dim)
+        I is intermediate_size (dimension of the hidden layer in MLP) (hidden_dim)
         L is the sequence length
 
         input: N.. x L x E
@@ -157,22 +157,99 @@ class Qwen2TransformerBlock:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.num_attention_heads = num_attention_heads
+        self.num_kv_heads = num_kv_heads
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.max_seq_len = max_seq_len
+
+        self.input_layernorm = RMSNorm(hidden_size, w_input_layernorm, rms_norm_eps)
+        self.self_attn = Qwen2MultiHeadAttention(hidden_size, num_attention_heads, num_kv_heads, wq, wk, wv, wo, bq, bk, bv, max_seq_len, theta)
+        self.post_attention_layernorm = RMSNorm(hidden_size, w_post_attention_layernorm, rms_norm_eps)
+        self.mlp = Qwen2MLP(hidden_size, intermediate_size, w_gate, w_up, w_down)
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        assert len(x.shape) >= 2, f"expect input x to have at least 2 dimensions, but get {len(x.shape)}."
+        assert x.shape[-1] == self.hidden_size, f"expect input x's last dimension to equal to {self.hidden_size}, but get {x.shape[-1]}."
+        assert x.shape[-1] <= self.max_seq_len, f"expect input x's sequence length to be less than or equal to max_seq_len ({self.max_seq_len}), but get {x.shape[-1]}."
+
+        x_attn = x + self.self_attn(self.input_layernorm(x), mask)
+        x_out = x_attn + self.mlp(self.post_attention_layernorm(x_attn))
+        return x_out
 
 
+from mlx_lm.models.qwen2 import Model
 class Qwen2ModelWeek1:
-    def __init__(self, mlx_model: Any):
-        pass
+    def __init__(self, mlx_model: Model):
+        self.precision = mx.float16 # use FP16 as compute precision
+
+        args = mlx_model.args
+        self.tie_word_embeddings = args.tie_word_embeddings
+
+        # helper function to get dequantized weight
+        def dq_weight(l: Any) -> mx.array:
+            return dequantize_linear(l).astype(self.precision)
+
+        w_embed = dq_weight(mlx_model.model.embed_tokens)
+        self.embed_tokens = Embedding(args.vocab_size, args.hidden_size, w_embed)
+
+        self.layers: List[Qwen2TransformerBlock] = []
+        for i in range(args.num_hidden_layers):
+            mlx_layer = mlx_model.model.layers[i]
+            mlx_mlp = mlx_layer.mlp
+            mlx_attn = mlx_layer.self_attn
+            layer = Qwen2TransformerBlock(
+                num_attention_heads=args.num_attention_heads,
+                num_kv_heads=args.num_key_value_heads,
+                hidden_size=args.hidden_size,
+                intermediate_size=args.intermediate_size,
+                rms_norm_eps=args.rms_norm_eps,
+                wq=dq_weight(mlx_attn.q_proj),
+                wk=dq_weight(mlx_attn.k_proj),
+                wv=dq_weight(mlx_attn.v_proj),
+                wo=dq_weight(mlx_attn.o_proj),
+                bq=mlx_attn.q_proj.bias.astype(self.precision),
+                bk=mlx_attn.k_proj.bias.astype(self.precision),
+                bv=mlx_attn.v_proj.bias.astype(self.precision),
+                w_gate=dq_weight(mlx_mlp.gate_proj),
+                w_up=dq_weight(mlx_mlp.up_proj),
+                w_down=dq_weight(mlx_mlp.down_proj),
+                w_input_layernorm=mlx_layer.input_layernorm.weight.astype(self.precision),
+                w_post_attention_layernorm=mlx_layer.post_attention_layernorm.weight.astype(self.precision),
+                max_seq_len=args.max_position_embeddings,
+                theta=int(args.rope_theta)
+            )
+            self.layers.append(layer)
+
+        w_norm = mlx_model.model.norm.weight.astype(self.precision)
+        self.norm = RMSNorm(args.hidden_size, w_norm, args.rms_norm_eps)
+
+        if not args.tie_word_embeddings:
+            self.w_lm_head = dq_weight(mlx_model.lm_head)
 
     def __call__(
         self,
         inputs: mx.array,
     ) -> mx.array:
-        pass
+        # get embbedings of input tokens
+        x = self.embed_tokens(inputs)
+
+        # go through transformer layers
+        for l in self.layers:
+            x = l(x, "causal")
+
+        # last rms norm
+        x = self.norm(x)
+
+        # get posibility distribution
+        if self.tie_word_embeddings:
+            prob = self.embed_tokens.as_linear(x)
+        else:
+            prob = linear(x, self.w_lm_head)
+
+        return prob
+
